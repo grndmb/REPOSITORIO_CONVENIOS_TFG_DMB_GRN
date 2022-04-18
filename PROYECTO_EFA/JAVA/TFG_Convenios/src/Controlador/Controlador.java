4d7895654adba7575364b1afa8c66758cabfb326
grndmb <proyectofinal.grn.dmb@gmail.com>
2022-04-18 16:32:53 +0200

    SAD

--- PROYECTO_EFA/JAVA/TFG_Convenios/src/Controlador/Controlador.java
+++ PROYECTO_EFA/JAVA/TFG_Convenios/src/Controlador/Controlador.java
@@ -1,7 +1,6 @@
 package Controlador;
 public class Controlador {
 	public static void main (String [] args) {
-		System.out.println("buenos dias");
-		System.out.println("Hola");
+		System.out.println("HOLAS DAGAOPU9IXVJKDSLVDWBVFDSBFDN");
 	}
 }
